58542f9e5a5203f01732dad692727f672368608c
JahrelStewart <48370051+JahrelStewart@users.noreply.github.com>
2021-03-14 18:55:28 -0400

    Added Refresh Functionality

--- script/productDescription.js
+++ script/productDescription.js
@@ -8,23 +8,35 @@ const inputQuantity = document.querySelector('.descriptionRow:nth-child(3) .inpu
 const minus = document.querySelector('.descriptionRow:nth-child(3) .plusMinus:nth-child(1)');
 const plus = document.querySelector('.descriptionRow:nth-child(3) .plusMinus:last-child');
 
+if (sessionStorage.getItem("inputQuantity")) {
+    inputQuantity.value = sessionStorage.getItem("inputQuantity");
+    price.childNodes[1].nodeValue = " $" + (price.getAttribute('data-value') * Math.max(inputQuantity.value, 1)).toFixed(2);
+    weight.childNodes[1].nodeValue = " " + (weight.getAttribute('data-value') * Math.max(inputQuantity.value, 1)) + weight.getAttribute('data-measure');
+}
+
 inputQuantity.addEventListener('input', () => {
     price.childNodes[1].nodeValue = " $" + (price.getAttribute('data-value') * Math.max(inputQuantity.value, 1)).toFixed(2);
     weight.childNodes[1].nodeValue = " " + (weight.getAttribute('data-value') * Math.max(inputQuantity.value, 1)) + weight.getAttribute('data-measure');
+
+    sessionStorage.setItem('inputQuantity', inputQuantity.value);
 });
 
 minus.addEventListener('click', () => {
-    const newMinus = inputQuantity.value - 1;
+    const newMinus = new Number(sessionStorage.getItem('inputQuantity')) - 1;
     inputQuantity.value = Math.max(newMinus, 1);
     price.childNodes[1].nodeValue = " $" + (price.getAttribute('data-value') * inputQuantity.value).toFixed(2);
     weight.childNodes[1].nodeValue = " " + (weight.getAttribute('data-value') * Math.max(inputQuantity.value, 1)) + weight.getAttribute('data-measure');
+
+    sessionStorage.setItem('inputQuantity', inputQuantity.value);
 });
 
 plus.addEventListener('click', () => {
-    const newPlus = new Number(inputQuantity.value) + 1;
-    inputQuantity.value = Math.max(newPlus, 1);
+    const newPlus = new Number(Math.max(sessionStorage.getItem('inputQuantity'), 1)) + 1;
+    inputQuantity.value = newPlus;
     price.childNodes[1].nodeValue = " $" + (price.getAttribute('data-value') * inputQuantity.value).toFixed(2);
     weight.childNodes[1].nodeValue = " " + (weight.getAttribute('data-value') * Math.max(inputQuantity.value, 1)) + weight.getAttribute('data-measure');
+
+    sessionStorage.setItem('inputQuantity', inputQuantity.value);
 });
 
 moreDescription.addEventListener('click', () => {
