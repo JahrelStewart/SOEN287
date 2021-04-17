536f6ed67b2ec65fdac45fa3ef265f7279d9c737
JahrelStewart <48370051+JahrelStewart@users.noreply.github.com>
2021-04-16 21:54:51 -0400

    prevent login when that user is not already in xml

---
+++ script/logIn.js
@@ -0,0 +1,49 @@
+const logInfo = document.querySelectorAll('.SignUpForm input');
+const logIn = document.querySelector('.SignUpForm .row3 .LogIn');
+logIn.style.pointerEvents = "none";
+
+getXML = new XMLHttpRequest();
+getXML.open("GET", "../xml/users.xml", true);
+getXML.responseType = 'document';
+// Force the response to be parsed as XML
+getXML.overrideMimeType('text/xml');
+
+getXML.onload = function () {
+    if (getXML.readyState === getXML.DONE && getXML.status === 200) {
+        let users = getXML.responseXML.querySelectorAll("users user");
+        let getEmail = getXML.responseXML.querySelectorAll("users user Email");
+        let getPassword = getXML.responseXML.querySelectorAll("users user Password");
+
+        logInfo.forEach(info => {
+            info.addEventListener('input', () => {
+                let goodLogIn = false;
+
+                if (logInfo[0].value != '' || logInfo[1].value != '') {
+                    for (let i = 0; i < users.length; i++) {
+                        if (logInfo[0].value == getEmail[i].innerHTML && logInfo[1].value == getPassword[i].innerHTML) {
+                            goodLogIn = true;
+                            break;
+                        }
+                    }
+
+                    if (goodLogIn == true) {
+                        logIn.style.pointerEvents = "auto";
+                        logIn.style.backgroundColor = "rgba(104, 176, 171, 0.50)";
+
+                    } else {
+                        logIn.style.pointerEvents = "none";
+                        logIn.style.backgroundColor = "rgba(208, 83, 83, 0.35)";
+                    }
+
+                } else {
+                    logIn.style.backgroundColor = "transparent";
+                }
+
+
+            });
+        });
+
+    }
+};
+
+getXML.send();
