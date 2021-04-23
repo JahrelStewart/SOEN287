60e0f614583525440ff82b190b47f90d595bdb5c
JahrelStewart <48370051+JahrelStewart@users.noreply.github.com>
2021-04-23 15:37:53 -0400

    Update logIn.js

--- script/logIn.js
+++ script/logIn.js
@@ -8,7 +8,7 @@ getXML.responseType = 'document';
 // Force the response to be parsed as XML
 getXML.overrideMimeType('text/xml');
 
-getXML.onload = function () {
+window.onload = function () {
     if (getXML.readyState === getXML.DONE && getXML.status === 200) {
         let users = getXML.responseXML.querySelectorAll("users user");
         let getEmail = getXML.responseXML.querySelectorAll("users user Email");
