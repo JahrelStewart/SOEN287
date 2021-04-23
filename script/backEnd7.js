30f2c2e08f8dbed2e13370d9bc17af5b8cfe5514
JahrelStewart <48370051+JahrelStewart@users.noreply.github.com>
2021-04-23 15:02:37 -0400

    Update backEnd7.js

--- script/backEnd7.js
+++ script/backEnd7.js
@@ -3,6 +3,15 @@ const deleteButton = document.querySelector('.addDeleteEditbutton .inner-addDele
 const productTable = document.querySelector('.tableList .tableBody');
 const productRows = document.querySelectorAll('.tableList .tableBody tr');
 
+window.addEventListener("pageshow", function (event) {
+    var historyTraversal = event.persisted ||
+        (typeof window.performance != "undefined" &&
+            window.performance.navigation.type === 2);
+    if (historyTraversal) {
+        window.location.reload();
+    }
+});
+
 editButton.style.pointerEvents = "none";
 deleteButton.style.pointerEvents = "none";
 
